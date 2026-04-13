1303349437ed51eaecbac98106902b0d2bc9637e
Victor Carvajal <ecos.moda.oficial@gmail.com>
2026-04-13 13:44:36 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,14 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const cormorant = Cormorant_Garamond({ 
   subsets: ['latin'], 
   weight: ['400', '700'], 
+  style: ['italic', 'normal'],
   variable: '--font-cormorant' 
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es" className={`${inter.variable} ${cormorant.variable}`}>
-      <body>{children}</body>
+    <html lang="es" className={`${inter.variable} ${cormorant.variable} scroll-smooth`}>
+      <body className="bg-iceGray antialiased">{children}</body>
     </html>
   );
 }
